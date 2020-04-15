5c1421c3b1d9dd8af032250890c3fb31f90f5700
KenLaw_ai <hklhfong01@gmail.com>
2020-04-15 18:01:10 +1000

    Question3 finished

--- CAB420_Assessment1A_Q4_Solution.py
+++ CAB420_Assessment1A_Q4_Solution.py
@@ -1,7 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 15:10:34 2020
+import os
+import glob
+import cv2
 
-@author: user
-"""
+path = r'C:\Users\user\Downloads\CAB420_Assessment1A_Data\Data\Q4\UTKFace\*.jpg'
+files = glob.glob(path)
 
+data = []
+for f in files:
+    d = {}
+    head, tail = os.path.split(f)
+    parts = tail.split('_')
+    if (len(parts) == 4):
+        d['age'] = int(parts[0])
+        d['gender'] = int(parts[1])
+        d['race'] = int(parts[2])
+        d['image'] = cv2.imread(f)
+        data.append(d)
+    else:
+        print('Could not load: ' + f + '! Incorrectly formatted filename')
+        
+
+
+    
+    
+    
